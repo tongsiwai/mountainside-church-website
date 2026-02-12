72053abfe92a9ff85143b0472a1e4b1f6281f3df
tongsiwai <tongsir@gmail.com>
2026-02-13 09:20:30 +1300

    Fix header overlap issue by adding top padding to main content

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <main className="flex-grow">
+      <main className="flex-grow pt-20">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
